4828b09230e32c93cb6e1a6fa8d0e5c0a0b22ddd
Samuel Olabode <solabode223@gmail.com>
2026-06-28 09:06:19 -0600

    feat: add CORS support and health check endpoint in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+const frontendOrigin = process.env.FRONTEND_ORIGIN;
+app.use(cors({origin: frontendOrigin}));
+
+app.get('/health', (_req, res) =>{
+    res.status(200).json({
+        status: 'okay',
+        message: 'Server is running!'
+    })
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () =>{
