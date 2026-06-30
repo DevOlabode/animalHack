feefc64175df56816c615fda14f989d5a63b50d1
Samuel Olabode <solabode223@gmail.com>
2026-06-30 11:28:17 -0600

    chore: update README.md to include installation instructions and usage examples

---
+++ backend/controllers/auth.js
@@ -0,0 +1,3 @@
+module.export.signIn = async ()=>{
+    
+}
---
+++ frontend/src/views/SignInView.jsx
@@ -0,0 +1,10 @@
+export default function SignInView () {
+    return (
+        <div>
+            <h2>Sign In To This App</h2>
+            <form action="rr">
+                <label htmlFor="username">UserName</label>
+            </form>
+        </div>
+    )
+}
